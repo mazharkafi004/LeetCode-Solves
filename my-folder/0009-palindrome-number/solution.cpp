1c93908a1baf37d39e7b20f9ca0b4210d8c167eb
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-03-08 11:14:47 +0000

    [LeetCode Sync] Runtime - 15 ms (19.15%), Memory - 5.9 MB (100.00%)

---
+++ my-folder/0009-palindrome-number/solution.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+        int length = s.length();
+        int mid = length / 2;
+        int count = 0;
+
+        for (int i = 0; i < mid; i++)
+        {
+            if (s[i] != s[length - (i + 1)])
+            {
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
