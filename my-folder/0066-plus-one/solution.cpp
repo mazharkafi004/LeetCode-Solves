26cd4cf97eaba1bbc932aa5e0ce8a49f0ab95bfe
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-03-13 17:45:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (100.00%)

---
+++ my-folder/0066-plus-one/solution.cpp
@@ -0,0 +1,77 @@
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        vector<int> ans;
+        ans = digits;
+        int len = digits.size();
+        int carry = 0;
+        if (digits[len - 1] == 9)
+        {
+            for (int i = len - 1; i >= 0; i--)
+            {
+
+                if (digits[i] == 9)
+                {
+                    if (i == 0)
+                    {
+                        if (len == 1)
+                        {
+                            ans[i] = 0;
+                            ans.insert(ans.begin(), 1);
+                        }
+                        else
+                        {
+                            if (carry == 0)
+                            {
+                                ans[i] = digits[i];
+                            }
+                            else
+                            {
+                                ans[i] = 0;
+                                ans.insert(ans.begin(), 1);
+                            }
+                        }
+                    }
+                    else if (i == len - 1)
+                    {
+                        ans[i] = 0;
+                        carry++;
+                    }
+
+                    else
+                    {
+                        if (carry == 0)
+                        {
+                            ans[i] = digits[i];
+                        }
+                        else
+                        {
+                            ans[i] = 0;
+                            carry++;
+                        }
+                    }
+                }
+                else
+                {
+
+                    if (carry == 0)
+                    {
+                        ans[i] = digits[i];
+                    }
+                    else
+                    {
+                        ans[i] = digits[i] + 1;
+                        carry = 0;
+                    }
+                }
+            }
+        }
+        else
+        {
+            ans[len - 1] = digits[len - 1] + 1;
+        }
+        return ans;
+    }
+};
