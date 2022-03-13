d2e57fde14cca021d78b11446b9015bf03454154
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-03-13 15:05:55 +0000

    Sync LeetCode submission - Sqrt(x) (cpp)

---
+++ problems/sqrt(x)/solution.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        int a = 0;
+        for (long long i = 0; i <= x; i++)
+        {
+            if ((i * i) == x)
+            {
+                return i;
+            }
+
+            else if ((i * i) > x)
+            {
+
+                return i - 1;
+            }
+        }
+        return 0;
+    }
+};
