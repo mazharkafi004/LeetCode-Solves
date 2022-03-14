59000af46e7a61a4e1fe390cea638cd276974986
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-03-14 09:19:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.6 MB (63.24%)

---
+++ my-folder/0071-simplify-path/solution.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+public:
+    string simplifyPath(string path)
+    {
+        stack<string> cannon_path;
+        string ans;
+
+        for (int i = 0; i < path.size(); ++i)
+        {
+            if (path[i] == '/')
+            {
+                continue;
+            }
+            string temp;
+
+            while (i < path.size() && path[i] != '/')
+            {
+
+                temp += path[i];
+                ++i;
+            }
+            if (temp == ".")
+            {
+                continue;
+            }
+
+            else if (temp == "..")
+            {
+                if (!cannon_path.empty())
+                    cannon_path.pop();
+            }
+            else
+            {
+
+                cannon_path.push(temp);
+            }
+        }
+
+        while (!cannon_path.empty())
+        {
+            ans = "/" + cannon_path.top() + ans;
+            cannon_path.pop();
+        }
+
+        if (ans.size() == 0)
+        {
+            return "/";
+        }
+
+        return ans;
+    }
+};
