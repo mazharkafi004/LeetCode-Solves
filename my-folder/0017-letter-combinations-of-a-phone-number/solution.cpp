d2a902d4203f95f23fd5a96080097c8a99144d44
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-03-16 09:22:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

---
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        vector<string> a{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> ans;
+        if (digits.length() == 0)
+        {
+            return ans;
+        }
+        ans.push_back("");
+        for (int i = 0; i < digits.length(); i++)
+        {
+            vector<string> temp;
+            string s = a[digits[i] - '2'];
+            for (int j = 0; j < s.length(); j++)
+            {
+                for (int k = 0; k < ans.size(); k++)
+                {
+                    temp.push_back(ans[k] + s[j]);
+                }
+            }
+            swap(temp, ans);
+        }
+
+        return ans;
+    }
+};
