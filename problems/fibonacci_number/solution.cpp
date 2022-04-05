f3a9aab91afcdd45bf42e0586a2968271b1ad937
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-05 11:36:23 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

---
+++ problems/fibonacci_number/solution.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        int f0 = 0, f1 = 1, f2 = 0;
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            for (int i = 2; i <= n; i++)
+            {
+
+                f2 = f0 + f1;
+                f0 = f1;
+                f1 = f2;
+            }
+            return f2;
+        }
+    }
+};
