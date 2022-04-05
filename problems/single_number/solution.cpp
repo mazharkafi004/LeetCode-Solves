e6b5663ea3221359e6954a619ca92666bd72dcc7
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-05 12:17:38 +0000

    Sync LeetCode submission - Single Number (cpp)

---
+++ problems/single_number/solution.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int a = -999999;
+        if (nums.size() == 1)
+        {
+            a = nums[0];
+        }
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1;)
+        {
+
+            if ((nums[i] != nums[i + 1]))
+            {
+                a = nums[i];
+                break;
+            }
+
+            else
+            {
+                i += 2;
+            }
+        }
+        if (a == -999999)
+        {
+            a = nums[nums.size() - 1];
+        }
+        return a;
+    }
+};
