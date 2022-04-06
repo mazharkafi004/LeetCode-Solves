9bb6a164c06d09f2dc14c25a49df065adace63bd
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-06 13:07:17 +0000

    [LeetCode Sync] Runtime - 31 ms (5.13%), Memory - 19.7 MB (99.95%)

---
+++ my-folder/0169-majority-element/solution.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        map<int, int> check;
+        int ans = 1, max = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int a = nums[i];
+            check[a]++;
+        }
+        for (auto i : check)
+        {
+            if (i.second > max)
+            {
+                max = i.second;
+                ans = i.first;
+            }
+        }
+        return ans;
+    }
+};
