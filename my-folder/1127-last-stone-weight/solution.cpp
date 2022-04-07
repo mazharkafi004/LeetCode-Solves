e64885bac7e79f967df4ae88dede49ca9aa1c130
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-07 19:05:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.6 MB (100.00%)

---
+++ my-folder/1127-last-stone-weight/solution.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    int lastStoneWeight(vector<int> &stones)
+    {
+        int a = 0;
+        while (stones.size() > 1)
+        {
+            if (a != 0)
+            {
+                break;
+            }
+            sort(stones.begin(), stones.end());
+
+            if (stones[stones.size() - 1] == stones[stones.size() - 2])
+            {
+
+                if (stones.size() == 2)
+                {
+                    a++;
+                    break;
+                }
+                stones.erase(stones.end() - 2);
+                stones.erase(stones.end() - 1);
+            }
+            else if (stones[stones.size() - 1] > stones[stones.size() - 2])
+            {
+
+                stones[stones.size() - 1] = stones[stones.size() - 1] - stones[stones.size() - 2];
+                stones.erase(stones.end() - 2);
+            }
+        }
+        if (a == 0)
+        {
+            return stones[0];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
