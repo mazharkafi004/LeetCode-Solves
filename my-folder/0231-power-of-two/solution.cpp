cb45ffc30bcf8657caaca004927dec50f18d58df
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-08 09:14:19 +0000

    [LeetCode Sync] Runtime - 4 ms (20.86%), Memory - 6 MB (100.00%)

---
+++ my-folder/0231-power-of-two/solution.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        int a = 0;
+        for (int i = 0; i <= sqrt(n) + 1; i++)
+        {
+            if (pow(2, i) == n)
+            {
+                a = 1;
+                break;
+            }
+        }
+        if (a == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
