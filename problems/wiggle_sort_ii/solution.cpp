aaa3cd60ef68a381dedb67dd4d1e65d614eba289
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-08 22:33:20 +0000

    Sync LeetCode submission - Wiggle Sort II (cpp)

---
+++ problems/wiggle_sort_ii/solution.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    void wiggleSort(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int p[nums.size()];
+        int l = nums.size() - 1;
+        for (int i = 1; i < nums.size(); i = i + 2)
+        {
+            p[i] = nums[l];
+            l--;
+        }
+        for (int i = 0; i < nums.size(); i = i + 2)
+        {
+            p[i] = nums[l];
+            l--;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nums[i] = p[i];
+        }
+    }
+};
