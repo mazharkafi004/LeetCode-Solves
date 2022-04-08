661caead8462ff604ff2ff123beaa773332607f9
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-08 09:45:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.8 MB (100.00%)

---
+++ my-folder/0228-summary-ranges/solution.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    vector<string> summaryRanges(vector<int> &nums)
+    {
+        vector<string> ans;
+        int a = 0;
+        int n = nums.size();
+        if (n == 0)
+        {
+            return ans;
+        }
+        else
+        {
+            a = nums[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (i == n - 1 || nums[i] != nums[i + 1] - 1)
+                {
+                    if (a != nums[i])
+                    {
+                        ans.push_back(to_string(a) + "->" + to_string(nums[i]));
+                    }
+                    else
+                    {
+                        ans.push_back(to_string(a));
+                    }
+                    if (i != n - 1)
+                    {
+                        a = nums[i + 1];
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+};
