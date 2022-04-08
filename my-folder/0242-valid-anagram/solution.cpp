148f2816560c075ea91a5f47c51e81e19bf41c45
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-08 15:55:44 +0000

    [LeetCode Sync] Runtime - 25 ms (6.80%), Memory - 7.3 MB (100.00%)

---
+++ my-folder/0242-valid-anagram/solution.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        int count = 0;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == t[i])
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+                break;
+            }
+        }
+        if (count == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+};
