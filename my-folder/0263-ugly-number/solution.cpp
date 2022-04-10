9b685d076003938d6cfb8e2567994650b08750ed
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-10 14:57:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.9 MB (100.00%)

---
+++ my-folder/0263-ugly-number/solution.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    bool isUgly(int n)
+    {
+        if (n < 1)
+        {
+            return false;
+        }
+        int a = 0;
+        while (n != 1)
+        {
+            if (n % 2 == 0)
+            {
+                n = n / 2;
+            }
+            else if (n % 3 == 0)
+            {
+                n = n / 3;
+            }
+            else if (n % 5 == 0)
+            {
+                n = n / 5;
+            }
+            else
+            {
+                a++;
+                break;
+            }
+        }
+        if(a == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
