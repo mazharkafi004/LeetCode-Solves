2792742e2f3ea44b95c4530443d0a683a1d7de7c
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-10 15:26:57 +0000

    Sync LeetCode submission - Missing Number (cpp)

---
+++ problems/missing_number/solution.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int a = 0;
+        for (int i = 0; i <= nums.size(); i++)
+        {
+            if (i == nums.size())
+            {
+                a = nums.size();
+                break;
+            }
+            if (i != nums[i])
+            {
+                a = i;
+                break;
+            }
+        }
+        return a;
+    }
+};
