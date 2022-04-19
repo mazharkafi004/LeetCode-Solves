99d0d457365deefa2140eee327a72a2178639e71
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-19 18:08:51 +0000

    [LeetCode Sync] Runtime - 3 ms (24.41%), Memory - 5.8 MB (100.00%)

---
+++ my-folder/0367-valid-perfect-square/solution.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool isPerfectSquare(int num)
+    {
+        for (long long i = 1;; i++)
+        {
+            if (i * i == num)
+            {
+                return true;
+            }
+            else if (i * i > num)
+            {
+                return false;
+            }
+        }
+    }
+};
