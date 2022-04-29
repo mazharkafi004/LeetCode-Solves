663b03e62e2617e98b0d8b259aab22fe56783425
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-29 13:54:57 +0000

    Sync LeetCode submission - Sort Even and Odd Indices Independently (cpp)

---
+++ problems/sort_even_and_odd_indices_independently/solution.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    vector<int> sortEvenOdd(vector<int> &nums)
+    {
+        vector<int> a(nums.size(), 0);
+        vector<int> c, d;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i % 2 == 0)
+            {
+                c.push_back(nums[i]);
+            }
+            else
+            {
+                d.push_back(nums[i]);
+            }
+        }
+        // return a;
+        sort(c.begin(), c.end());
+
+       sort(d.begin(), d.end(), greater<int>());
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i % 2 == 0)
+            {
+                a[i] = c[i / 2];
+            }
+            else
+            {
+                a[i] = d[i / 2];
+            }
+        }
+        return a;
+    }
+};
