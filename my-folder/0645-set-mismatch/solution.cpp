0be52e77507dbf7b26458d0aa9b1634b2d4480ae
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-04-30 16:59:52 +0000

    [LeetCode Sync] Runtime - 29 ms (44.32%), Memory - 21.4 MB (100.00%)

---
+++ my-folder/0645-set-mismatch/solution.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
+        vector<int> a(2, 0);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i > 0)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    a[0] = nums[i];
+
+                    nums.erase(nums.begin() + i);
+                    break;
+                }
+            }
+        }
+        for (int i = 1; i <= nums.size(); i++)
+        {
+            if (i != nums[i - 1])
+            {
+                a[1] = i;
+                break;
+            }
+        }
+        if (a[1] == 0)
+        {
+            a[1] = nums[nums.size() - 1] + 1;
+        }
+
+        return a;
+    }
+};
