0831f1fff0b1d7c1ef4c22ce32efd60198a5e575
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-01 13:20:00 +0000

    [LeetCode Sync] Runtime - 7 ms (25.04%), Memory - 6.3 MB (100.00%)

---
+++ my-folder/0874-backspace-string-compare/solution.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+public:
+    bool backspaceCompare(string s, string t)
+    {
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (i > 0)
+            {
+                if (s[i] == '#')
+                {
+
+                    s.erase(i - 1, 2);
+                    i = i - 2;
+                }
+            }
+            else
+            {
+                if (s[i] == '#')
+                {
+                    s.erase(i, 1);
+                    i--;
+                }
+            }
+        }
+        for (int i = 0; i < t.length(); i++)
+        {
+            if (i > 0)
+            {
+                if (t[i] == '#')
+                {
+                    t.erase(i - 1, 2);
+                    i = i - 2;
+                }
+            }
+            else
+            {
+                if (t[i] == '#')
+                {
+
+                    t.erase(i, 1);
+                    i--;
+                }
+            }
+        }
+        if (s == t)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
