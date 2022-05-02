9b19cdeebb716b4dfbc3571db36bf749e1371eb0
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-02 08:58:33 +0000

    Sync LeetCode submission - Sort Array By Parity (cpp)

---
+++ problems/sort_array_by_parity/solution.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    vector<int> sortArrayByParity(vector<int> &nums)
+    {
+        vector<int> odd, even, ans(nums.size(), 0);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                even.push_back(nums[i]);
+            }
+            else
+            {
+                odd.push_back(nums[i]);
+            }
+        }
+        for (int i = 0; i < even.size(); i++)
+        {
+            ans[i] = even[i];
+        }
+        for (int i = 0; i < odd.size(); i++)
+        {
+            ans[i+even.size()] = odd[i];
+        }
+        return ans;
+    }
+};
