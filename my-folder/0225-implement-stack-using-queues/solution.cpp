ea0ae2991d95362f1c3137a3b897322a698cbfaf
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-05 11:34:04 +0000

    [LeetCode Sync] Runtime - 4 ms (9.08%), Memory - 6.9 MB (100.00%)

---
+++ my-folder/0225-implement-stack-using-queues/solution.cpp
@@ -0,0 +1,45 @@
+class MyStack
+{
+public:
+    queue<int> queue;
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        queue.push(x);
+    }
+
+    int pop()
+    {
+        for (int i = 0; i < queue.size() - 1; i++)
+        {
+            int a = queue.front();
+            queue.pop();
+            queue.push(a);
+        }
+        int b = queue.front();
+        queue.pop();
+        return b;
+    }
+
+    int top()
+    {
+        return queue.back();
+    }
+
+    bool empty()
+    {
+        return queue.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
