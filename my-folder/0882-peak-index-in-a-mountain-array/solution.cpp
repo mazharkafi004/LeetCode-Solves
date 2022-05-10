1b5d64a5c8d9d911ec9c945871ce284a18d91231
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-10 08:27:12 +0000

    [LeetCode Sync] Runtime - 17 ms (99.99%), Memory - 11.5 MB (100.00%)

---
+++ my-folder/0882-peak-index-in-a-mountain-array/solution.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int l = 1, r = arr.size() - 1, a = 0;
+        while (l <= r)
+        {
+            int mid = (r - l) / 2 + l;
+            if (arr[mid] < arr[mid + 1])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+};
