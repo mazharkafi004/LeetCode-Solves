c4f63013123a3afa725fcdfc3e323d33e275b523
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-12 14:10:31 +0000

    [LeetCode Sync] Runtime - 21 ms (6.11%), Memory - 15.9 MB (100.00%)

---
+++ my-folder/0745-find-smallest-letter-greater-than-target/solution.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    char nextGreatestLetter(vector<char> &letters, char target)
+    {
+        char a;
+        int count = 0;
+        int check = target - 'a';
+        for (int i = 0; i < letters.size(); i++)
+        {
+            int value = (letters[i] - 'a');
+            if (value > check)
+            {
+                count++;
+                a = letters[i];
+                break;
+            }
+        }
+        if (count == 0)
+        {
+            a = letters[0];
+        }
+        return a;
+    }
+};
