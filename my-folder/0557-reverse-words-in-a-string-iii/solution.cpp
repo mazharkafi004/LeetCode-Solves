8cde7216be88eecdba49ac60d32c8eddf45b5e66
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-12 09:27:48 +0000

    [LeetCode Sync] Runtime - 20 ms (26.23%), Memory - 9.7 MB (100.00%)

---
+++ my-folder/0557-reverse-words-in-a-string-iii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        int l = 0, r = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+
+            if (s[i] == ' ')
+            {
+
+                reverse(s.begin() + l, s.begin() + i);
+                l = i + 1;
+            }
+        }
+        reverse(s.begin() + l, s.end());
+        return s;
+    }
+};
