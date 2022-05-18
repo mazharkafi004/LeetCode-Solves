36691cdfb047710920aad0d3d32a4a93a19cdf2c
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-18 09:58:48 +0000

    Sync LeetCode submission - Sum of Square Numbers (cpp)

---
+++ problems/sum_of_square_numbers/solution.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    bool judgeSquareSum(int c)
+    {
+        if (c < 3)
+        {
+            return true;
+        }
+        long long a = 0, b = sqrt(c);
+        while (a <= b)
+        {
+            if ((a * a) + (b * b) == c)
+            {
+                return true;
+            }
+            else if ((a * a) + (b * b) > c)
+            {
+                b--;
+            }
+            else
+            {
+                a++;
+            }
+        }
+        return false;
+    }
+};
