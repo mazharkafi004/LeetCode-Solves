8351c3591d79f5d0ee0ecb6e1ecd0827bb6ac0d0
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-18 09:49:46 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (cpp)

---
+++ problems/intersection_of_two_arrays_ii/solution.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> ans;
+        int m = nums1.size(), i = 0;
+        int n = nums2.size(), j = 0;
+        while (i < m && j < n)
+        {
+            if (nums1[i] == nums2[j])
+            {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return ans;
+    }
+};
