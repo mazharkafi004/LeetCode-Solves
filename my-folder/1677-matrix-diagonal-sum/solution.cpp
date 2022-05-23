84e68fc9e9fd2c68819a4c205ec47f737495d83e
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-23 14:07:35 +0000

    [LeetCode Sync] Runtime - 24 ms (18.43%), Memory - 11.3 MB (100.00%)

---
+++ my-folder/1677-matrix-diagonal-sum/solution.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int diagonalSum(vector<vector<int>> &mat)
+    {
+        long long n = mat.size(), m = mat[0].size() - 1, ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if ((i) == (m - i))
+            {
+                ans += mat[i][m - i];
+            }
+            else
+            {
+                ans += mat[i][m - i] + mat[i][i];
+            }
+            
+        }
+        return ans;
+    }
+};
