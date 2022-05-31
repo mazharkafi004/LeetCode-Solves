ebc842cb10ca86f3795bf36f7d1398fdc93cfbfb
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-05-31 16:19:57 +0000

    [LeetCode Sync] Runtime - 142 ms (14.59%), Memory - 61.3 MB (99.95%)

---
+++ my-folder/0287-find-the-duplicate-number/solution.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int l = 1, r = nums.size() - 1;
+        while (l <= r)
+        {
+            int mid = (r - l) / 2 + l;
+            int count = 0;
+            for (int n : nums)
+            {
+                if (n <= mid)
+                    ++count;
+            }
+            if (count <= mid)
+            {
+                l = mid + 1;
+            }
+            else if (count > mid)
+            {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+};
