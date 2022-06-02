7d8e17e8b87875e024e90a0807a45434540fea5a
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-02 17:46:40 +0000

    Sync LeetCode submission - Keep Multiplying Found Values by Two (cpp)

---
+++ problems/keep_multiplying_found_values_by_two/solution.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int findFinalValue(vector<int> &nums, int original)
+    {
+
+        while (true)
+        {
+            sort(nums.begin(), nums.end());
+            int l = 0, r = nums.size() - 1, check = 0;
+            while (l <= r)
+            {
+                int mid = (r - l) / 2 + l;
+                if (nums[mid] == original)
+                {
+                    original += original;
+                    nums[mid] += original;
+
+                    check++;
+                    break;
+                }
+                else if (nums[mid] < original)
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+            if (check == 0)
+            {
+                break;
+            }
+        }
+        return original;
+    }
+};
