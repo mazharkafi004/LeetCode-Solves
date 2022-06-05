058d4970b9b58cb593a55120e779845a655488c3
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-05 18:52:29 +0000

    [LeetCode Sync] Runtime - 47 ms (6.32%), Memory - 24.1 MB (49.06%)

---
+++ my-folder/0958-sort-array-by-parity-ii/solution.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    vector<int> sortArrayByParityII(vector<int> &nums)
+    {
+        vector<int> even, odd, ans;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                even.push_back(nums[i]);
+            }
+            else
+            {
+                odd.push_back(nums[i]);
+            }
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i % 2 == 0)
+            {
+                ans.push_back(even[i / 2]);
+            }
+            else
+            {
+                ans.push_back(odd[i / 2]);
+            }
+        }
+        return ans;
+    }
+};
