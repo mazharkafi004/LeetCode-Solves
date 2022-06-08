5381e77f076361d11e305dc1901f5ad3dab49869
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-08 15:13:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.2 MB (100.00%)

---
+++ my-folder/1454-remove-palindromic-subsequences/solution.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int removePalindromeSub(string s)
+    {
+
+        for (int i = 0; i < s.length() / 2; i++)
+        {
+            if (s[i] != s[(s.length() - 1) - i])
+            {
+                return 2;
+            }
+        }
+        return 1;
+    }
+};
