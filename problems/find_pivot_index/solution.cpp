839d982619ef336d33adaee3782d1109b96a5874
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-14 20:37:25 +0000

    Sync LeetCode submission - Find Pivot Index (cpp)

---
+++ problems/find_pivot_index/solution.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+        int leftsum = 0, rightsum = 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            rightsum += nums[i];
+        }
+        int l = 0, r = 1;
+        while (l < nums.size())
+        {
+            if (leftsum == rightsum)
+            {
+                return l;
+            }
+            else
+            {
+                if (l + 1 == nums.size() - 1)
+                {
+                    leftsum += nums[l];
+                    rightsum = 0;
+                    if (leftsum == rightsum)
+                    {
+                        return l+1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    leftsum += nums[l];
+
+                    rightsum -= nums[l + 1];
+                    l++;
+                }
+            }
+        }
+        return -1;
+    }
+};
