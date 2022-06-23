98506ee7b57f735f94c2c3cf4f98aa8f3bce26b7
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-23 16:36:48 +0000

    Sync LeetCode submission - Linked List Cycle (cpp)

---
+++ problems/linked_list_cycle/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        unordered_set<ListNode *> node_count;
+        while (head != NULL && !node_count.count(head))
+        {
+            node_count.insert(head);
+            head = head->next;
+        }
+        return head;
+    }
+};
