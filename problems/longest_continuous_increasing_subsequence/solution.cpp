7bd756e685f40295ca204a61c7c13a21cc544c77
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-23 09:00:03 +0000

    Sync LeetCode submission - Longest Continuous Increasing Subsequence (cpp)

---
+++ problems/longest_continuous_increasing_subsequence/solution.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int findLengthOfLCIS(vector<int> &nums)
+    {
+        int l = 0, r = 1, s = l, count = 1, mx = INT_MIN;
+        while (l < nums.size() - 1)
+        {
+
+            while (r < nums.size())
+            {
+
+                if (nums[r] > nums[s])
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+                s++;
+                r++;
+            }
+            mx = max(count, mx);
+            count = 1;
+            l++;
+            s = l;
+            r = l + 1;
+        }
+        return max(mx, 1);
+    }
+};
