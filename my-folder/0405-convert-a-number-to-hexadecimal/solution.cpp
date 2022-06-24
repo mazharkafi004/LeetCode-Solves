5c71d1ba19adb431ef9f1a668c714fae51dd586e
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-24 18:39:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.2 MB (100.00%)

---
+++ my-folder/0405-convert-a-number-to-hexadecimal/solution.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    string toHex(int num)
+    {
+        map<int, char> dig;
+        char digit = '0';
+        char a = 'a';
+        for (int i = 0; i <= 15; i++)
+        {
+            if (i >= 10)
+            {
+                dig[i] = a++;
+            }
+            else
+            {
+                dig[i] = digit++;
+            }
+        }
+        string ans = "";
+        if (num == 0)
+        {
+            return "0";
+        }
+        else if (num > 0)
+        {
+            while (num != 0)
+            {
+                ans = dig[num % 16] + ans;
+                num /= 16;
+            }
+        }
+        else
+        {
+            u_int n = num;
+            while (n != 0)
+            {
+                ans = dig[n % 16] + ans;
+                n /= 16;
+            }
+        }
+        return ans;
+    }
+};
