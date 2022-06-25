06b79325f7c0cacad70cb54a917a24110cf3fcfd
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-25 14:48:41 +0000

    Sync LeetCode submission - 2 Keys Keyboard (cpp)

---
+++ problems/2_keys_keyboard/solution.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int minSteps(int n)
+    {
+        int ans = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            while (n % i == 0)
+            {
+                ans += i;
+                n = n / i;
+            }
+        }
+        return ans;
+    }
+};
