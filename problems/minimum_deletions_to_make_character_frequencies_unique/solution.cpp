71407d39c8b4ce80fdd10055a965904b13273cd3
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-28 14:54:35 +0000

    Sync LeetCode submission - Minimum Deletions to Make Character Frequencies Unique (cpp)

---
+++ problems/minimum_deletions_to_make_character_frequencies_unique/solution.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int minDeletions(string s)
+    {
+        vector<int> count(26, 0);
+        for (int k = 0; k < s.size(); k++)
+        {
+            count[s[k] - 'a']++;
+        }
+        int ans = 0;
+        sort(count.begin(), count.end());
+        for (int i = 24; i >= 0; i--)
+        {
+            if (count[i] == 0)
+            {
+                break;
+            }
+            if (count[i] >= count[i + 1])
+            {
+                int a = count[i];
+                count[i] = max(0, count[i + 1] - 1);
+                ans += a - count[i];
+            }
+        }
+        return ans;
+    }
+};
