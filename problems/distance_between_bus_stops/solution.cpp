0970a5a92544cbbf1d7367abb62ecde1e8bc5fd9
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-29 00:46:39 +0000

    Sync LeetCode submission - Distance Between Bus Stops (cpp)

---
+++ problems/distance_between_bus_stops/solution.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int distanceBetweenBusStops(vector<int> &distance, int start, int destination)
+    {
+        int left_dist = 0, right_dist = 0;
+        if (destination < start)
+        {
+            swap(destination, start);
+        }
+        for (int i = 0; i < distance.size(); i++)
+        {
+            if (i >= start && i < destination)
+            {
+                left_dist += distance[i];
+            }
+            else
+            {
+                right_dist += distance[i];
+            }
+        }
+        return min(left_dist, right_dist);
+    }
+};
