156f08ae7d85df688ec92f0199ae6f1cbb00d187
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-06-30 13:24:02 +0000

    [LeetCode Sync] Runtime - 28 ms (11.14%), Memory - 11 MB (100.00%)

---
+++ my-folder/0462-minimum-moves-to-equal-array-elements-ii/solution.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int minMoves2(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int mid = nums.size() / 2, ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans += abs(nums[mid] - nums[i]);
+        }
+        return ans;
+    }
+};
