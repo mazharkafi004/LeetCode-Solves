4070dabafc2e8e10ae4c917ca47aadb2e00c6811
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-07-03 12:10:43 +0000

    [LeetCode Sync] Runtime - 3 ms (58.88%), Memory - 7.1 MB (100.00%)

---
+++ my-folder/0376-wiggle-subsequence/solution.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int wiggleMaxLength(vector<int> &nums)
+    {
+        int l = 0, r = 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                l = r + 1;
+            }
+            else if (nums[i] < nums[i - 1])
+            {
+                r = l + 1;
+            }
+        }
+        return max(l + 1, r + 1);
+    }
+};
