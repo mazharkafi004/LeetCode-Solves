bd1bb21d6c59674eacda67f94c892320e32a8025
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-07-27 13:01:27 +0000

    Sync LeetCode submission - Number of Burgers with No Waste of Ingredients (cpp)

---
+++ problems/number_of_burgers_with_no_waste_of_ingredients/solution.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices)
+    {
+        vector<int> ans;
+        // return ans;
+        int cheese1 = cheeseSlices, cheese2 = 0;
+
+        while (cheese1 >= 0)
+        {
+            if (((cheese1 * 2) + (cheese2 * 4)) == tomatoSlices)
+            {
+                return {cheese2,cheese1};
+            }
+            else
+            {
+                cheese1--;
+                cheese2++;
+            }
+        }
+        return ans;
+    }
+};
