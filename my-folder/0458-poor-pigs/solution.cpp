d63b9dccc79ed33bf9c6177e7da1a1ca2080c2e6
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-08-06 20:37:14 +0000

    [LeetCode Sync] Runtime - 3 ms (11.09%), Memory - 6.2 MB (100.00%)

---
+++ my-folder/0458-poor-pigs/solution.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest)
+    {
+        int max = 10, check = (minutesToTest / minutesToDie) + 1;
+        for (int i = 0; i < max; i++)
+        {
+            if (pow(check, i) >= buckets)
+            {
+                return i;
+            }
+        }
+        return max;
+    }
+};
