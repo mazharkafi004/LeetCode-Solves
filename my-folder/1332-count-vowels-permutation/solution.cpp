62996ddc0ef096f4a632faf125ad652b160f800d
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-08-07 12:01:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.8 MB (100.00%)

---
+++ my-folder/1332-count-vowels-permutation/solution.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int countVowelPermutation(int n)
+    {
+        long long a = 1, e = 1, i = 1, o = 1, u = 1, modulo = pow(10, 9) + 7;
+        long long a2 = 0, e2 = 0, i2 = 0, o2 = 0, u2 = 0;
+
+        for (int k = 2; k <= n; k++)
+        {
+            a2 = (e + i + u) % modulo;
+            e2 = (a + i) % modulo;
+            i2 = (e + o) % modulo;
+            o2 = i;
+            u2 = (o + i) % modulo;
+
+            a = a2, e = e2, i = i2, o = o2, u = u2;
+        }
+
+        return (a + e + i + o + u) % modulo;
+    }
+};
