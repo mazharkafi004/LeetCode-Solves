f46ebff363fa4f2d62672cca54578115487bc350
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-08-09 12:07:22 +0000

    [LeetCode Sync] Runtime - 7 ms (87.42%), Memory - 10.5 MB (100.00%)

---
+++ my-folder/0300-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        vector<int> ans;
+        for (int i : nums)
+        {
+            if (ans.empty() || ans[ans.size() - 1] < i)
+            {
+                ans.push_back(i);
+            }
+            else
+            {
+                auto it = lower_bound(ans.begin(), ans.end(), i); 
+                *it = i;                                        
+            }
+        }
+        return ans.size();
+    }
+};
