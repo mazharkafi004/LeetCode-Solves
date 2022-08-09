10b1300627aed3c8d5f6fb34833c91ba0f481818
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-08-09 13:21:04 +0000

    [LeetCode Sync] Runtime - 318 ms (5.02%), Memory - 71.1 MB (92.48%)

---
+++ my-folder/0940-fruit-into-baskets/solution.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int totalFruit(vector<int> &fruits)
+    {
+        unordered_map<int, int> ans;
+        int start = 0, end = 0, mx = 0;
+        while (end < fruits.size())
+        {
+            ans[fruits[end]]++;
+             while(ans.size()>2)
+            {
+                if(ans[fruits[start]]==1)
+                {
+                    ans.erase(fruits[start]);
+                }
+                else
+                {
+                    ans[fruits[start]]--;
+                }
+                start++;
+            }
+            mx = max(mx, end - start + 1);
+            end++;
+        }
+        return mx;
+    }
+};
