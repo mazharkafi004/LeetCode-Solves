29dd5af62547e1a56afe4cb1b00673ae24b44f4f
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2022-09-12 05:33:36 +0000

    Sync LeetCode submission - Bag of Tokens (cpp)

---
+++ problems/bag_of_tokens/solution.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int bagOfTokensScore(vector<int> &tokens, int power)
+    {
+        sort(tokens.begin(), tokens.end());
+        int i = 0, j = tokens.size(), n = j, score = 0;
+        while (i < j)
+        {
+            if (tokens[i] <= power)
+            {
+                power -= tokens[i];
+                i++;
+                score++;
+            }
+            else if (j > i + 1 && score)
+            {
+                --j;
+                power += tokens[j];
+                score--;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return score;
+    }
+};
