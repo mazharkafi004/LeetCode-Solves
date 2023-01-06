5b7da1992b80f3beebf46299449ec6adb560c849
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-01-06 10:31:57 +0000

    [LeetCode Sync] Runtime - 226 ms (5.36%), Memory - 76.4 MB (100.00%)

---
+++ my-folder/1961-maximum-ice-cream-bars/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int count = 0;
+        for(auto cost: costs) {
+            if(cost<=coins){
+                count++;
+                coins-=cost;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+};
