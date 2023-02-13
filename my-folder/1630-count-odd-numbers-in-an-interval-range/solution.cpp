bed9510a3173a5e3655bcc66d19a60860cba1906
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-02-13 17:25:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.9 MB (100.00%)

--- my-folder/1630-count-odd-numbers-in-an-interval-range/solution.cpp
+++ my-folder/1630-count-odd-numbers-in-an-interval-range/solution.cpp
@@ -1,15 +1,8 @@
-class Solution
-{
+class Solution {
 public:
-    int countOdds(int low, int high)
-    {
-        if (low % 2 == 0 && high % 2 == 0)
-        {
-            return ((high - low) / 2);
-        }
-        else
-        {
-            return ((high - low) / 2) + 1;
-        }
+    int countOdds(int low, int high) {
+        int dif = high-low;
+        bool ck = low%2 + high % 2;
+        return (dif/2)  + (int) ck;
     }
 };
