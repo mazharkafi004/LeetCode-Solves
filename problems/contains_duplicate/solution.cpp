b065d2e5546e52e57c0f6617b70f3d5ba3cbb874
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-03-05 16:56:51 +0000

    Sync LeetCode submission - Contains Duplicate (cpp)

--- problems/contains_duplicate/solution.cpp
+++ problems/contains_duplicate/solution.cpp
@@ -4,7 +4,8 @@ public:
     bool containsDuplicate(vector<int> &nums)
     {
         map<int, long long> answer;
-        for (int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+        for (int i = 0; i < n ; i++)
         {
             answer[nums[i]]++;
         }
