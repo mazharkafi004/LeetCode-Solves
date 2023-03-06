2f93cc23fd74c9346fd9b45a9c5ef69fd4a2ce86
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-03-06 16:34:35 +0000

    Sync LeetCode submission - First Bad Version (cpp)

--- problems/first_bad_version/solution.cpp
+++ problems/first_bad_version/solution.cpp
@@ -1,21 +1,16 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-class Solution
-{
+class Solution {
 public:
-    int firstBadVersion(int n)
-    {
+    int firstBadVersion(int n) {
         int l = 1, r = n;
-        while (l < r)
-        {
+        while(l<r){
             int mid = (r-l)/2+l;
-            if (isBadVersion(mid) == true)
-            {
+            if(isBadVersion(mid)){
                 r = mid;
             }
-            else
-            {
+            else {
                 l = mid + 1;
             }
         }
