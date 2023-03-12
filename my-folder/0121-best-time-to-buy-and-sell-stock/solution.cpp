b7bff00012392c35894c98cb82f8d53eb915b716
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-03-12 16:33:16 +0000

    [LeetCode Sync] Runtime - 131 ms (5.24%), Memory - 93.3 MB (99.84%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,24 +1,16 @@
-class Solution
-{
+class Solution {
 public:
-    int maxProfit(vector<int> &prices)
-    {
-        int max_profit = 0, current_profit = 0;
-        int l = 0, r = 1;
-        while (r < prices.size())
-        {
-
-            current_profit = prices[r] - prices[l];
-            if (current_profit < 0)
-            {
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 1, n = prices.size(), current_profit = 0, max_profit = 0;
+        while(r<n){
+            current_profit = prices[r]-prices[l];
+            if(current_profit<0){
                 l = r;
             }
-
-            r++;
-            if (current_profit > max_profit)
-            {
+            if(current_profit>max_profit){
                 max_profit = current_profit;
             }
+            r++;
         }
         return max_profit;
     }
