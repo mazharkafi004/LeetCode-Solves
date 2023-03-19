dd6912273a22f100c29b4fc353f0815bfbcc83b0
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-03-19 08:58:41 +0000

    [LeetCode Sync] Runtime - 3 ms (31.94%), Memory - 5.9 MB (100.00%)

--- my-folder/0191-number-of-1-bits/solution.cpp
+++ my-folder/0191-number-of-1-bits/solution.cpp
@@ -1,14 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    int hammingWeight(uint32_t n)
-    {
-        int count = 0;
-        while (n > 0)
-        {
+    int hammingWeight(uint32_t n) {
+       int count = 0;
+       while(n>0){
+           if(n&1==1){
             count++;
-            n = n & (n - 1);
-        }
-        return count;
+           }
+           n = n>>1;
+       }
+       return count;
     }
 };
