50628d6ff2c3ca49b9672bfc3b40dc4e44c363be
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-04-01 19:31:38 +0000

    Sync LeetCode submission - Sort the Students by Their Kth Score (cpp)

---
+++ problems/sort_the_students_by_their_kth_score/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    static bool comp(vector<int>& s1, vector<int>& s2, int tmp) {
+        return s1[tmp] > s2[tmp];
+    }
+    
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        sort(score.begin(), score.end(), [&](vector<int>& s1, vector<int>& s2) {
+            return comp(s1, s2, k);
+        });
+        return score;
+    }
+};
