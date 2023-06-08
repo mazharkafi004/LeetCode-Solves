e235890261276cab70113299683174cd3400173f
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-06-08 16:48:59 +0000

    [LeetCode Sync] Runtime - 21 ms (21.68%), Memory - 16.8 MB (99.99%)

--- my-folder/0136-single-number/solution.cpp
+++ my-folder/0136-single-number/solution.cpp
@@ -1,32 +1,10 @@
-class Solution
-{
+class Solution {
 public:
-    int singleNumber(vector<int> &nums)
-    {
-        int a = -999999;
-        if (nums.size() == 1)
-        {
-            a = nums[0];
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0;i<nums.size();i++){
+            ans ^= nums[i];
         }
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size() - 1;)
-        {
-
-            if ((nums[i] != nums[i + 1]))
-            {
-                a = nums[i];
-                break;
-            }
-
-            else
-            {
-                i += 2;
-            }
-        }
-        if (a == -999999)
-        {
-            a = nums[nums.size() - 1];
-        }
-        return a;
+        return ans;
     }
 };
