5317351c9dae71892fed4197e22c1755f8d00d6a
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-08-03 21:23:03 +0000

    Sync LeetCode submission - Zigzag Conversion (cpp)

---
+++ problems/zigzag_conversion/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows <= 1){
+            return s;
+            }
+
+        vector<string> vec(numRows, "");
+        int n = s.length(), pos = 0, prev = -1;
+        for (int i = 0; i < n; i++) {
+            vec[pos] += s[i];
+            if (pos == 0) {
+                pos++;
+                prev = pos - 1;
+            } else if (pos == numRows - 1) { 
+                pos--;
+                prev = numRows - 1;
+            } else if (pos > prev) {
+                pos++;
+                prev = pos - 1;
+            } else {
+                pos--;
+                prev = pos + 1;
+            }
+        }
+        string t;
+        for (auto v : vec) {
+            t += v;
+        }
+        return t;
+    }
+};
