8bfd09679b093d974c8bb513a0de89e59fb0a1a2
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-11-27 16:21:04 +0000

    Sync LeetCode submission - Reverse Words in a String (cpp)

--- problems/reverse_words_in_a_string/solution.cpp
+++ problems/reverse_words_in_a_string/solution.cpp
@@ -1,29 +1,34 @@
+#include <vector>
+#include <string>
+
 class Solution {
 public:
     string reverseWords(string s) {
         int n = s.length();
-        vector<string>vec;
+        vector<string> vec;
         string t;
-        for(int i=0;i<n;i++){
-            if(s[i]==' '){
-                if(t.length()>0){
-                  vec.push_back(t);
-                  t = "";
+
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == ' ') {
+                if (!t.empty()) {
+                    vec.push_back(t);
+                    t.clear();  // Instead of t = "";
                 }
-            }
-            else{
-                t+=s[i];
+            } else {
+                t += s[i];
             }
         }
-        if(!t.empty()){
+
+        if (!t.empty()) {
             vec.push_back(t);
         }
+
         string ans;
-        for(int i=vec.size()-1;i>0;i--){
-            ans+=vec[i];
-            ans+=" ";
+        for (int i = vec.size() - 1; i > 0; --i) {
+            ans += vec[i] + " ";  // Concatenate directly with a space
         }
-        ans+=vec[0];
+        ans += vec[0];
+
         return ans;
     }
 };
