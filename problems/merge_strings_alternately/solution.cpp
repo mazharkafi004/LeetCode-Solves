a0e86f8c30f7a8ed887d397388fbfb1a5f7a7e8b
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-11-27 13:07:18 +0000

    Sync LeetCode submission - Merge Strings Alternately (cpp)

--- problems/merge_strings_alternately/solution.cpp
+++ problems/merge_strings_alternately/solution.cpp
@@ -1,37 +1,17 @@
-class Solution
-{
+class Solution {
 public:
-    string mergeAlternately(string word1, string word2)
-    {
-        int m = word1.length(), n = word2.length();
-        string ans(m + n, 'z');
-
-        for (int i = 0; i <= (min(n, m) * 2); i++)
-        {
-            if (i % 2 == 0)
-            {
-                ans[i] = word1[i / 2];
+    string mergeAlternately(string word1, string word2) {
+        int pos = 0;
+        string s = "";
+        while(pos<word1.length() || pos<word2.length()){
+            if(pos<word1.length()){
+                s+=word1[pos];
             }
-            else
-            {
-                ans[i] = word2[i / 2];
+             if(pos<word2.length()){
+                s+=word2[pos];
             }
-        }
-        if (m > n)
-        {
-            for (int i = 0; i < m - n; i++)
-            {
-                ans[i + (n*2)] = word1[n + i];
-            }
-        }
-        else if (m < n)
-        {
-            for (int i = 0; i < n - m; i++)
-            {
-                ans[i + (m*2)] = word2[m + i];
-            }
-        }
-
-        return ans;
-    }
+            pos++;
+      }
+      return s;  
+      }
 };
