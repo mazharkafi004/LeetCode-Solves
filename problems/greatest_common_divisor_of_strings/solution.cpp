00b2ae648a44e969a268c94b759a01eba5a32946
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2023-11-27 13:14:35 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (cpp)

--- problems/greatest_common_divisor_of_strings/solution.cpp
+++ problems/greatest_common_divisor_of_strings/solution.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-    if(str1+str2==str2+str1){
-     int gcd = __gcd(str1.length(),str2.length());
-     return str1.substr(0,gcd);
+        return (str1+str2==str2+str1)?str1.substr(0,__gcd(str1.length(),str2.length())):"";
     }
-    else{
-     return "";
-    }
-    }
-
 };
