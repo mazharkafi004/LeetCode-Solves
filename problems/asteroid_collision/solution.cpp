4cd552ee9a36780fcf12290b743b8b75673834a9
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2024-04-16 09:50:09 +0000

    Sync LeetCode submission - Asteroid Collision (cpp)

--- problems/asteroid_collision/solution.cpp
+++ problems/asteroid_collision/solution.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int>st;
-        int n = asteroids.size();
-        for(int i=0;i<n;i++){
-          if(asteroids[i]>0 || st.empty()){
-              st.push(asteroids[i]);
-          }
-          else{
-              while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])){
-                  st.pop();
-              }
-              if(!st.empty() && st.top() == abs(asteroids[i])){
-                  st.pop();
-              }
-              else{
-                  if(st.empty() || st.top()<0){
-                      st.push(asteroids[i]);
-                  }
-              }
-
-          }
+        for(int i=0;i<asteroids.size();i++){
+            if(st.empty() || asteroids[i]>0){
+                st.push(asteroids[i]);
+            }
+            else{
+                while(!st.empty() && st.top()>0 && abs(asteroids[i])>st.top()){
+                    st.pop();
+                }
+                if(!st.empty() && abs(asteroids[i])==st.top()){
+                    st.pop();
+                }
+                else{
+                    if(st.empty() || st.top()<0){
+                        st.push(asteroids[i]);
+                    }
+                }
+            }
+            
         }
-         vector<int> vec;
-         while (!st.empty()) {
-          vec.push_back(st.top());
-          st.pop();
-         }
-        reverse(vec.begin(), vec.end());
-        return vec;
+            vector<int>vec;
+            while(!st.empty()){
+                vec.push_back(st.top());
+                st.pop();
+            }
+            reverse(vec.begin(),vec.end());
+            return vec;
     }
 };
