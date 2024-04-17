b0e8954c28322ed6d5268e0eecfa99ea92e752f1
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2024-04-17 05:49:24 +0000

    [LeetCode Sync] Runtime - 3 ms (92.57%), Memory - 17.7 MB (31.76%)

--- my-folder/0104-maximum-depth-of-binary-tree/solution.cpp
+++ my-folder/0104-maximum-depth-of-binary-tree/solution.cpp
@@ -11,14 +11,28 @@
  */
 class Solution {
 public:
-   
+    int bfs(TreeNode* root){
+        int mx = 0;
+        queue<pair<TreeNode*,int>>queue;
+        queue.push({root,1});
+        while(!queue.empty()){
+            auto [node,depth] = queue.front();
+            queue.pop();
+            mx = max(mx,depth);
+            if(node->left){
+                queue.push({node->left,depth+1});
+            }
+            if(node->right){
+                queue.push({node->right,depth+1});
+            }
+        }
+        return mx;
+    }
     int maxDepth(TreeNode* root) {
         if(root==NULL){
             return 0;
         }
-        
-        return max(1+ maxDepth(root->left), 1+ maxDepth(root->right));
-         
+       int ans =  bfs(root);
+       return ans;
     }
-    
 };
