52c1532f0f41b69fb55cb93dba90bdf0f6f87541
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2024-04-19 11:13:25 +0000

    [LeetCode Sync] Runtime - 27 ms (76.14%), Memory - 14.8 MB (69.71%)

--- my-folder/1807-partitioning-into-minimum-number-of-deci-binary-numbers/solution.cpp
+++ my-folder/1807-partitioning-into-minimum-number-of-deci-binary-numbers/solution.cpp
@@ -1,13 +1,14 @@
-class Solution
-{
+class Solution {
 public:
-    int minPartitions(string n)
-    {
-        int mx = INT_MIN;
-        for (int i = 0; i < n.size(); i++)
-        {
-            int digit_value = n[i] - '0';
-            mx = max(mx, digit_value);
+    int minPartitions(string n) {
+        int mx = 0;
+        for (int i=0;i<n.size();i++){
+            if(mx==9){
+                return 9;
+            }
+            if(n[i]-'0'>mx){
+                mx = n[i] - '0';
+            }
         }
         return mx;
     }
