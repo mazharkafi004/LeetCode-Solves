ec56698d3684a59952cbac25580c17f91ba019e3
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2024-05-23 11:40:55 +0000

    Sync LeetCode submission - To Be Or Not To Be (javascript)

--- problems/to_be_or_not_to_be/solution.js
+++ problems/to_be_or_not_to_be/solution.js
@@ -1,20 +1,26 @@
 /**
- * @param {any} val
+ * @param {string} val
  * @return {Object}
  */
-const expect = (val) => ({
-    toBe: val2 => {
-        if (val === val2) {
-            return true ;
-        }  else{
-        throw new Error ("Not Equal") ;
-        }
-    },
-    notToBe: (val2) => {
-        if (val !== val2) {
-            return true ;
-        } else{
-        throw new Error ("Equal") ;
+var expect = function(val) {
+    let n = val;
+    return {
+        toBe: function(new_val){
+            if(n===new_val){
+                return true;
+            }
+           throw new Error("Not Equal")
+        },
+        notToBe: function(new_val){
+            if(n!==new_val){
+                return true;
+            }
+            throw new Error("Equal")
         }
     }
-});
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
