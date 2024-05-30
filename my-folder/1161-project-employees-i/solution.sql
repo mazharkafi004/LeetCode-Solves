d2bfc56011bcccf0bbfd385cda4641ed738cfa18
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2024-05-30 04:15:18 +0000

    [LeetCode Sync] Runtime - 1051 ms (6.55%), Memory - 0B (100.00%)

--- my-folder/1161-project-employees-i/solution.sql
+++ my-folder/1161-project-employees-i/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select project_id, round(avg(employee.experience_years),2) as average_years 
-from project
-join employee on project.employee_id = employee.employee_id
-group by project_id
+select p.project_id, round(avg(e.experience_years),2) as average_years
+from  Project p join Employee e on p.employee_id = e.employee_id
+where e.experience_years  is not null
+group by p.project_id
