eb9b05f24792755f9e98e5cf0e5169e6f927e61c
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2024-07-13 11:08:07 +0000

    [LeetCode Sync] Runtime - 6 ms (21.08%), Memory - 8.2 MB (36.31%)

--- my-folder/0389-find-the-difference/solution.cpp
+++ my-folder/0389-find-the-difference/solution.cpp
@@ -1,17 +1,13 @@
-class Solution
-{
+class Solution {
 public:
-    char findTheDifference(string s, string t)
-    {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (s[i] != t[i])
-            {
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<s.size();i++){
+            if(s[i] != t[i]){
                 return t[i];
             }
         }
-        return t[t.length() - 1];
+        return t[t.size()-1];
     }
 };
