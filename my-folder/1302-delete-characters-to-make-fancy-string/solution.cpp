2bc51241accd6271994a55748a16ff241cbdb722
Md. Mazharul Islam <43183733+mazharkafi004@users.noreply.github.com>
2024-11-01 16:44:51 +0000

    [LeetCode Sync] Runtime - 29 ms (49.74%), Memory - 42.8 MB (63.16%)

---
+++ my-folder/1302-delete-characters-to-make-fancy-string/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        int count = 1;
+        string result;
+        result+=s[0];
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1]){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            if(count<3){
+                result+=s[i];
+            }
+        }
+        return result;
+    }
+};
